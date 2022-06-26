41699a0c89e31b20415819bac36793063564f00d
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-06-26 18:40:56 +0500

    fixed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,11 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a;
-        int b;
-        a = 1;
-        b = 2;
-        double y = (9*Math.pow(a, 2) - 5*b + 2 + a - 7) * ((a + b - 4.0*a*b) / 2);
+        double a = 1.0;
+        double b = 2.0;
+        double y = (9*Math.pow(a, 2) - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
         System.out.println(y);
     }
 }
